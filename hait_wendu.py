3c756d7fd457500fdfc70e84b6d750fe52a7f19c
wangxiaolei <77225404+wangxiaolei21@users.noreply.github.com>
2024-07-12 23:17:42 +0800

    Update hait_wendu.py

--- hait_wendu.py
+++ hait_wendu.py
@@ -1,26 +1,28 @@
 from datetime import datetime, timezone, timedelta
 import requests, json
 import hashlib
- 
+
+
 def sha256_hash(s):
     return hashlib.sha256(s.encode('utf-8')).hexdigest()
- 
-def get_token(user,pwd):
-    login_url=r"https://api.ubibot.cn/accounts/login"
-    shuju={
+
+
+def get_token(user, pwd):
+    login_url = r"https://api.ubibot.cn/accounts/login"
+    shuju = {
         "expire_in_seconds": "2592000",
         "password": sha256_hash(pwd),
         "password_type": "sha256",
         "username": user
     }
-    jieguo=requests.post(url=login_url, data=shuju).json()
-    print("token_id:",jieguo['token_id'])
-    print("过期时间:",jieguo['expire_time'])
+    jieguo = requests.post(url=login_url, data=shuju).json()
+    print("token_id:", jieguo['token_id'])
+    print("过期时间:", jieguo['expire_time'])
     return jieguo['token_id']
- 
- 
+
+
 def send_message(xiaoxi: str, quantizhaohuan=0):
-    dingding_url = r"https://oapi.dingtalk.com/robot/send?access_token=76b01a6c051-----------158e6cd3b3ef948631be776c1a90b5d16d457bb3"
+    dingding_url = r"https://oapi.dingtalk.com/robot/send?access_token=76b01a6c051e6fe73651c631158e6cd3b3ef948631be776c1a90b5d16d457bb3"
     tou = {"Content-Type": "application/json; charset=utf-8"}
     neirong = {"msgtype": "text",
                "text": {"content": xiaoxi},
@@ -33,20 +35,21 @@ def send_message(xiaoxi: str, quantizhaohuan=0):
     print(messagebody)
     result = requests.post(url=dingding_url, data=messagebody, headers=tou)
     print(result.text)
- 
-def make_message(token:str):
+
+
+def make_message(token: str):
     # 定义UTC+8时区
     tz_utc_8 = timezone(timedelta(hours=8))
     # 获取东8区的当前日期和时间
     now_utc_8 = datetime.now(tz_utc_8)
     before_15fenzhong = now_utc_8 + timedelta(minutes=-15)
- 
+
     # 打印年月日
     jieshu = now_utc_8.strftime('%Y-%m-%d %H:%M:%S')
     kaishi = before_15fenzhong.strftime('%Y-%m-%d %H:%M:%S')
- 
+
     wangzhi = "http://api.ubibot.cn/channels/5375/feeds?end=" + jieshu + "&field=field1&" \
-                     "start=" + kaishi + "&timezone=Asia/Shanghai&token_id="+token
+                                                                         "start=" + kaishi + "&timezone=Asia/Shanghai&token_id=" + token
     print(wangzhi)
     jieguo = requests.get(url=wangzhi).json()
     if 'error' in jieguo.values():
@@ -60,9 +63,10 @@ def make_message(token:str):
             jg = "温度：%s (%s)" % (wendu, jieshu)
             print(jg)
     send_message(jg, 0)
- 
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
     yonghuming = "zhenweiw@qq.com"
     mima = "jkxjkx309"
-    token=get_token(yonghuming,mima)
+    token = get_token(yonghuming, mima)
     make_message(token)
